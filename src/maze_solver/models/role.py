d7d45e09720ab89e03d56bc7797e8e8312c518f1
Ricardo Montañana <rmontanana@gmail.com>
2023-03-22 17:53:49 +0100

    first commit

---
+++ src/maze_solver/models/role.py
@@ -0,0 +1,13 @@
+# models/role.py
+
+from enum import IntEnum, auto
+
+
+class Role(IntEnum):
+    NONE = 0
+    ENEMY = auto()
+    ENTRANCE = auto()
+    EXIT = auto()
+    EXTERIOR = auto()
+    REWARD = auto()
+    WALL = auto()
---
+++ src/maze_solver/models/solution.py
@@ -0,0 +1,34 @@
+# models/solution.py
+
+from dataclasses import dataclass
+from typing import Iterator
+from functools import reduce
+
+from maze_solver.models.square import Square
+from maze_solver.models.role import Role
+
+
+@dataclass(frozen=True)
+class Solution:
+    squares: tuple[Square, ...]
+
+    def __post_init__(self) -> None:
+        assert self.squares[0].role is Role.ENTRANCE, "Wrong entrance"
+        assert self.squares[-1].role is Role.EXIT, "Wrong exit"
+        reduce(validate_corridor, self.squares)
+
+    def __iter__(self) -> Iterator[Square]:
+        return iter(self.squares)
+
+    def __getitem__(self, index: int) -> Square:
+        return self.squares[index]
+
+    def __len__(self) -> int:
+        return len(self.squares)
+
+
+def validate_corridor(current: Square, following: Square) -> Square:
+    assert any(
+        [current.row == following.row, current.column == following.column]
+    ), "Squares must lie in the same row or column"
+    return following
